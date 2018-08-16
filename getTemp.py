412d1e5c0eabf2191f82e896198619f2b01abb23
Rich <lpeaua@gmail.com>
2018-08-16 02:23:50 -0600

    sdf

--- getTemp.py
+++ getTemp.py
@@ -1,13 +1,26 @@
 #! python3
 # currentTemp.py - display the current temperature
 
+#! python3
+# currentTemp.py - display the current temperature
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+
+# Prompt for location of desired temperature
+print("Please enter city and state:\n")
+city = input('City: ')
+state = input("State: ")
+country = input("Country: ")
+# Open National Weather website and grab location search bar
 browser = webdriver.Firefox()
+browser.get("https://www.weather.gov/")
+linkElem = browser.find_element_by_id('inputstring')
 
-browser.get("http://inventwithpython.com")
-
-try:
-    elem = browser.find_element_by_class_name('bookcover')
-    print('Found <%s> element with that call name!' % (elem.tag_name))
-except:
-    print('Was not able to find an element with that name.')
+# Clear existing text in search bar, insert inputted location and navigate to location site
+linkElem.clear()
+linkElem.send_keys(city.title() + ', ' + state.upper() + ', ' +country.upper()).click()
+#linkElem.send_keys(Keys.ENTER)
+buttonElem = browser.find_element_by_id('btnSearch').click()
+#buttonElem.send_keys(Keys.ENTER)
