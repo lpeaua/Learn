c26f6fa7e0bb12d4e15696e62f1c7b431e91b88f
Rich <lpeaua@gmail.com>
2018-08-17 10:39:55 -0600

    sdf

--- getTemp.py
+++ getTemp.py
@@ -12,15 +12,14 @@ import sys
 print("Please enter city and state:\n")
 city = input('City: ')
 state = input("State: ")
-country = input("Country: ")
 # Open National Weather website and grab location search bar
 browser = webdriver.Firefox()
-browser.get("https://www.weather.gov/")
-linkElem = browser.find_element_by_id('inputstring')
+browser.get("https://weather.com/")
+linkElem = browser.find_element_by_class_name('theme__inputElement__4bZUj input__inputElement__1GjGE')
 
-# Clear existing text in search bar, insert inputted location and navigate to location site
+# Clear existing text in search bar,  insert inputted location and navigate to location site
 linkElem.clear()
-linkElem.send_keys(city.title() + ', ' + state.upper() + ', ' +country.upper(), Keys.ENTER)
+linkElem.send_keys(city.title() + ', ' + state.upper(), Keys.ENTER)
 #linkElem.submit()
 #buttonElem = browser.find_element_by_name('getForecast')
 #buttonElem.submit()
